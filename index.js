8a77d9dad2a95cf086125c3a137e0f3c4eb2b059
TonyMacaroni <Tonygiuliani3502@gmail.com>
2023-08-01 15:03:08 -0500

    finished file layout and index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const inquirer = require("inquirer");
+const fs = require("fs")
+const { generateSVG } = require("./Assets/GenerateSVG");
+const { generateShape } = require('./Assets/GenerateShape');
+const { error } = require("console");
+
+inquirer 
+.prompt([
+    {
+        type: 'input',
+        name: 'logoName',
+        message: 'Please enter 1-3 characters for your logo name',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Please enter color keyword or hexadecimal for your logos text color',
+    },
+    {
+        type: 'input',
+        name: 'logoColor',
+        message:'Please enter color keyword or hexadecimal for your logos background color',
+    },
+    {
+            type: 'list',
+            name: 'logoShape',
+            message: 'Please choose a shape for your logo',
+        choices: ['Triangle', 'Square,', 'Circle'], 
+    }
+])
+.then((data) => {
+    const svgPath = './Assets/GeneratedLogo.svg';
+    const finalLogo = generateShape(data);
+
+
+    fs.writeFile(svgPath, generateSVG(finalLogo), (err) =>
+    err ? console.error(err) : console.log("Generated Logo")
+    );
+})
+.catch((err) => console.log(err));
